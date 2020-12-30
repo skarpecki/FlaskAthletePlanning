7e24169c183a29865c6a13d631b90c96ffe78f34
Szymon <skarpecki@outlook.com>
2020-12-31 00:03:06 +0100

    Logging sent emails

--- app/__init__.py
+++ app/__init__.py
@@ -11,16 +11,15 @@ handler = EmailHandler("localhost", 1025, "me@me.pl", context)
 db = SQLAlchemy()
 jwt = JWTManager()
 
-def create_app(test_config=None) -> 'Flask app':
 
-    
+def create_app(test_config=None) -> 'Flask app':
     app = Flask(__name__)
     api = Api(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:user@localhost/athlete_planning'
     app.config['JWT_SECRET_KEY'] = 'dev'
     db.init_app(app)
     jwt.init_app(app)
-
+    handler.init_db(db)
 
     from app.users import controller as users_controller
     api.add_resource(users_controller.Users, '/users')
@@ -29,15 +28,13 @@ def create_app(test_config=None) -> 'Flask app':
     from app.trainings import controller as trainings_controller
     api.add_resource(trainings_controller.TrainingsSearch, '/trainings')
     api.add_resource(trainings_controller.Trainings, '/trainings/<int:training_id>')
-    #api.add_resource(trainings_controller.Exercises, '/exercises')
+    # api.add_resource(trainings_controller.Exercises, '/exercises')
     api.add_resource(trainings_controller.Exercises, '/trainings/<int:training_id>/exercises')
     api.add_resource(trainings_controller.ExercisesWithID, '/trainings/<int:training_id>/exercises/<int:exercise_id>')
-
 
     # me = model.Athlete(id=1)
     # with app.app_context():
     #     db.session.add(me)
     #     db.session.commit()
-
 
     return app
--- app/common/messenger.py
+++ app/common/messenger.py
@@ -1,9 +1,9 @@
 import smtplib, ssl
 from datetime import date, datetime
 
+
 # command to start smtp local server to fetch messages
 # python -m smtpd -c DebuggingServer -n localhost:1025
-
 
 
 
@@ -13,22 +13,46 @@ class EmailHandler:
         self.port = port
         self.sender = sender
         self.context = context
+        self.db = None
 
     def run_local(self):
         server = smtplib.SMTP(self.smtp_server, self.port)
         server.ehlo()
         return server
 
+    def init_db(self, db):
+        self.db = db
+
+    def log_mail(self, addresse, subject, message):
+        if self.db is not None:
+            metadata = self.db.MetaData()
+            metadata.bind = self.db.engine
+            mail_logs = self.db.Table("mail_logs", metadata,
+                                      self.db.Column("mail_addresse", self.db.String(100), nullable=False),
+                                      self.db.Column("subject", self.db.String(100), nullable=False),
+                                      self.db.Column("message", self.db.String(255), nullable=False),
+                                      self.db.Column("created_utc", self.db.DateTime, nullable=False))
+            ins = mail_logs.insert().values(mail_addresse=addresse,
+                                           subject=subject,
+                                           message=message,
+                                           created_utc=datetime.utcnow())
+            conn = self.db.engine.connect()
+            result = conn.execute(ins)
+            else:
+            raise TypeError("Database was not initialized")
+
     def new_training_msg(self, addresse, training, coach):
         try:
             server = self.run_local()
             subject = "Subject: New training scheduled on {}\n\n".format(str(training.date))
             message = "New training of id {} scheduled on {} by coach {} {}".format(training.id, str(training.date),
-                                                                                    str(coach.first_name), str(coach.last_name))
+                                                                                    str(coach.first_name),
+                                                                                    str(coach.last_name))
             server.sendmail(self.sender, addresse, subject + message)
+            self.log_mail(addresse, subject, message)
         except Exception as e:
             server = None
-            print(e)
+            print("new_training_msg: {}".format(str(e)))
         finally:
             if server is not None:
                 server.close()
@@ -40,9 +64,10 @@ class EmailHandler:
             message = "Training of id {} rescheduled from {} to {}".format(training.id, str(old_date.date()),
                                                                            str(training.date.date()))
             server.sendmail(self.sender, addresse, subject + message)
+            self.log_mail(addresse, subject, message)
         except Exception as e:
             server = None
-            print(e)
+            print("updated_train_date_msg: {}".format(str(e)))
         finally:
             if server is not None:
                 server.close()
@@ -53,13 +78,13 @@ class EmailHandler:
             subject = "Subject: New exercise scheduled on {}\n\n".format(str(_date))
             message = "Details of exercise: \n{}".format(str(exercise))
             server.sendmail(self.sender, addresse, subject + message)
+            self.log_mail(addresse, subject, message)
         except Exception as e:
             server = None
-            print(e)
+            print("new_exercise_msg: {}".format(str(e)))
         finally:
             if server is not None:
                 server.close()
-
 
     def update_exercise_msg(self, addresse, _date, old_exercise, new_exercise):
         try:
@@ -67,10 +92,11 @@ class EmailHandler:
             subject = "Subject: Exercise scheduled on {} was modified\n\n".format(str(_date))
             message = "Old exercise details:\n{}\n" \
                       "New exercise details:\n{}\n".format(str(old_exercise), str(new_exercise))
-            server.sendmail(self.sender, addresse, subject+message)
+            server.sendmail(self.sender, addresse, subject + message)
+            self.log_mail(addresse, subject, message)
         except Exception as e:
             server = None
-            print(e)
+            print("update_exercise_msg: {}".format(str(e)))
         finally:
             if server is not None:
                 server.close()
@@ -84,14 +110,14 @@ if __name__ == "__main__":
         "exerciseName": "deadlift",
         "sets": 10,
         "rpe": 7.0
-        }
+    }
 
     _new_fields = {
         "reps": 5,
         "exerciseName": "deadlift",
         "sets": 5,
         "rpe": 7.0
-        }
+    }
 
     handler.new_exercise_msg("athlete@athlete.com", date(2020, 1, 1), _fields)
     handler.update_exercise_msg("athlete@athlete.com", date(2020, 1, 1), _fields, _new_fields)
